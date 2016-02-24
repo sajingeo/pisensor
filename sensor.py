dcce177ea5daec009eded308a7c4beaad50a28ad
Sajin George <Sajin George>
2016-02-24 00:50:53 -0500

    Added support for starting car, using amazon echo

--- sensor.py
+++ sensor.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
-# Example of using the MQTT client class to subscribe to and publish feed values.
-# Author: Tony DiCola
+""" 
+Sensory.py this is a drivative of Fauxmo.py, example-minimal.py
+This emulates a weemo device on the network and also pushes sensor data to Adafruit IO.
+The weemo device name can be set in the script and the can be found by an amazon echo.
+"""
 
-# Import standard python modules.
+import fauxmo
+import logging
+import time
+import RPi.GPIO as GPIO
 import random
 import sys
 import time
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 from twilio.rest import TwilioRestClient
+from debounce_handler import debounce_handler
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(10, GPIO.IN)
+GPIO.setup(9,GPIO.OUT)
 runOnce = 0
 securityEnable = 'OFF'
 
@@ -21,12 +29,13 @@ from Adafruit_IO import MQTTClient
 from Adafruit_IO import Client
 
 # Set to your Adafruit IO key & username below.
-ADAFRUIT_IO_KEY      = 'XXXXXXXXXXXXXXXXXX'
-ADAFRUIT_IO_USERNAME = 'superman'  # See https://accounts.adafruit.com
-account_sid = "ACXXXXXXXXXXXXXXXXX"
-auth_token  = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+ADAFRUIT_IO_KEY      = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
+ADAFRUIT_IO_USERNAME = 'sXXXXXXo'  # See https://accounts.adafruit.com
+account_sid = "XXXXXXXXXXXXXXXXX"
+auth_token  = "XXXXXXXXXXXXXXXXXXX"
 # to find your username.
 
+GPIO.output(9, GPIO.LOW) #make sure you dont start the car
 
 def presenseHandle(channel):
     # print 'presense detected!!'
@@ -35,7 +44,7 @@ def presenseHandle(channel):
     if (securityEnable.value == 'ON'):
         #print 'Send Alarm' ## you can add your notification here TWILLIO/ GMAIL etc
         SMSclient = TwilioRestClient(account_sid, auth_token)
-        message = SMSclient.messages.create(body="Motion Detected!!!",to="+1XXXXXXXXXX",from_="+XXXXXXXXXXXX")
+        message = SMSclient.messages.create(body="Motion Detected!!!",to="+15857667935",from_="+19783194030")
 
 # Define callback functions which will be called when certain events happen.
 def connected(client):
@@ -58,58 +67,68 @@ def message(client, feed_id, payload):
     # the new value.
     print 'Feed {0} received new value: {1}'.format(feed_id, payload)
 
-# Create an MQTT client instance.
-client=MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-restClient = Client(ADAFRUIT_IO_KEY)
+logging.basicConfig(level=logging.CRITICAL)
 
-# Setup the callback functions defined above.
-client.on_connect=connected
-client.on_disconnect=disconnected
-client.on_message=message
+class device_handler(debounce_handler):
+    """Publishes the on/off state requested,
+       and the IP address of the Echo making the request.
+    """
+    TRIGGERS = {"My Car": 52000}
 
-# Connect to the Adafruit IO server.
-client.connect()
+    def act(self, client_address, state):
+        print "State", state, "from client @", client_address
+        if(state):
+            print "turning car ON"
+            GPIO.output(9,GPIO.HIGH)
+            time.sleep(4)
+            
+        GPIO.output(9,GPIO.LOW)
+        return True
 
-# Now the program needs to use a client loop function to ensure messages are
-# sent and received.  There are a few options for driving the message loop,
-# depending on what your program needs to do.  
+if __name__ == "__main__":
+    # Startup the fauxmo server
+    fauxmo.DEBUG = True
+    timeout = 0
+    p = fauxmo.poller()
+    u = fauxmo.upnp_broadcast_responder()
+    u.init_socket()
+    p.add(u)
 
-# The first option is to run a thread in the background so you can continue
-# doing things in your program.
-# client.loop_background()
-# Now send new values every 10 seconds.
-# print 'Publishing a new message every 10 seconds (press Ctrl-C to quit)...'
-while True:
-    if(runOnce == 0):
-        runOnce = 1
-        GPIO.add_event_detect(10, GPIO.RISING, callback=presenseHandle,bouncetime=10000)
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,4)
-    if humidity is not None and temperature is not None:
-        # print 'Humi:'+str(humidity)+'Tmp:'+str(temperature)
-        client.publish('Temp', temperature)
-        client.publish('Humidity', humidity)
-	client.publish('Presense',0)
-    time.sleep(10)
-# Another option is to pump the message loop yourself by periodically calling
-# the client loop function.  Notice how the loop below changes to call loop
-# continuously while still sending a new message every 10 seconds.  This is a
-# good option if you don't want to or can't have a thread pumping the message
-# loop in the background.
-#last = 0
-#print 'Publishing a new message every 10 seconds (press Ctrl-C to quit)...'
-#while True:
-#   # Explicitly pump the message loop.
-#   client.loop()
-#   # Send a new message every 10 seconds.
-#   if (time.time() - last) >= 10.0:
-#       value = random.randint(0, 100)
-#       print 'Publishing {0} to DemoFeed.'.format(value)
-#       client.publish('DemoFeed', value)
-#       last = time.time()
+    # Register the device callback as a fauxmo handler
+    d = device_handler()
+    for trig, port in d.TRIGGERS.items():
+        fauxmo.fauxmo(trig, u, p, None, port, d)
 
-# The last option is to just call loop_blocking.  This will run a message loop
-# forever, so your program will not get past the loop_blocking call.  This is
-# good for simple programs which only listen to events.  For more complex programs
-# you probably need to have a background thread loop or explicit message loop like
-# the two previous examples above.
-#client.loop_blocking()
+    # Loop and poll for incoming Echo requests
+    logging.debug("Entering fauxmo polling loop")
+    client=MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+    restClient = Client(ADAFRUIT_IO_KEY)
+
+    # Setup the callback functions defined above.
+    client.on_connect=connected
+    client.on_disconnect=disconnected
+    client.on_message=message
+
+    # Connect to the Adafruit IO server.
+    client.connect()
+
+    while True:
+        try:
+            # Allow time for a ctrl-c to stop the process
+            p.poll(100)
+            if(runOnce == 0):
+                runOnce = 1
+                GPIO.add_event_detect(10, GPIO.RISING, callback=presenseHandle,bouncetime=10000)
+            timeout = timeout + 1
+            if (timeout >= 100):
+                timeout = 0
+                humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,4)
+                if humidity is not None and temperature is not None:
+                    # print 'Humi:'+str(humidity)+'Tmp:'+str(temperature)
+                    client.publish('Temp', temperature)
+                    client.publish('Humidity', humidity)
+                    client.publish('Presense',0)
+            time.sleep(0.1)
+        except Exception, e:
+            logging.critical("Critical exception: " + str(e))
+            break
